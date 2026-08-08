9aae3e8950a77347db61df8ae9a7dc18501f8305
leonatz17 <natzyleon@gmail.com>
2026-08-08 14:40:08 +0800

    404

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
     Page Not Found
 </h1>
 
-<h1><?php the_title(); ?></h1>
-<h2><?php the_content(); ?></h2>
+<!-- <h1><?php the_title(); ?></h1>
+<h2><?php the_content(); ?></h2> --> 
 
 <?php get_footer(); ?>
